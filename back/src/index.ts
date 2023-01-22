8e2b8c1db2c68f247f83c2d81a016a58a8d39129
strawberrystr1 <andrey_overdose@mail.ru>
2023-01-22 19:42:57 +0300

    feat: add socket package

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,46 +1,19 @@
+import ws from "ws";
 import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import bodyParser from "body-parser";
 
-import router from "./routes";
-import dbClient from "./db";
+const app = express();
 
-const startServer = () => {
-  dotenv.config();
+const wsServer = new ws.Server({
+  server: app.listen(3000),
+  host: "localhost",
+  path: "/"
+});
 
-  const port = process.env.PORT || "4000";
-  const app = express();
+wsServer.on("connection", socket => {
+  socket.on("message", msg => {
+    console.log("meesage received", msg);
+    socket.send("hello from server");
+  });
+});
 
-  app.use(
-    cors({
-      origin: "*"
-    })
-  );
-  app.use(express.json());
-  app.use(
-    bodyParser.urlencoded({
-      extended: false
-    })
-  );
-  app.use(bodyParser.json());
-
-  app.use(router);
-
-  const runDB = async () => {
-    try {
-      await dbClient.authenticate();
-      await dbClient.sync({ alter: true });
-
-      app.listen(port, () => {
-        console.log(`server started at port ${port}`);
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  runDB();
-};
-
-startServer();
+app.listen( () => console.log("server is ups"));
--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react';
+
 function App() {
+  const ws = new WebSocket('ws://localhost:3000');
+  useEffect(() => {
+    ws.onopen = () => console.log('connection open');
+
+    ws.onmessage = (msg: MessageEvent<string>) => console.log(msg);
+  }, []);
   return (
     <div className="App">
-      asd
+      <button onClick={() => ws.send('hello from front')}>asdasd</button>
     </div>
   );
 }
