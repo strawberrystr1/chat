6feddfb378d1ca207e4b84ed807b5a37f9446c3f
Andrei Shakh <andrey_overdose@mail.ru>
2023-01-24 12:23:25 +0300

    fix: fix wss

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://astonishing-dinner-production.up.railway.app';
 export const JSON_HEADERS = {
   'Content-Type': 'application/json',
 };
-export const SOCKET_URL = 'ws://astonishing-dinner-production.up.railway.app/';
+export const SOCKET_URL = 'wss://astonishing-dinner-production.up.railway.app/';
